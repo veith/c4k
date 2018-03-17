da366608147bb0eefdb7ee51c74413b32d119540
veith <veithz@gmail.com>
2018-03-17 14:25:36 +0100

    assets game-dice init

--- docupdate.sh
+++ docupdate.sh
@@ -3,7 +3,7 @@
 
 echo "Aktualisiere app-doc"
 cp ./.docshell bower_components/app-doc/index.html
-polymer analyze ./shell/**.html > bower_components/app-doc/analysis.json
+polymer analyze ./src/**.html > bower_components/app-doc/analysis.json
 
 echo "App documentation: http://127.0.0.1:8000/components/app-doc/"
 
